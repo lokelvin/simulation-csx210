6647b46e46fc95a5fb6b5435e83e3188acc67e8c
mepcotterell@gmail.com <mepcotterell@gmail.com@bfff1a8a-6af3-b283-1508-272d5eb36651>
2011-08-31 22:53:38 +0000

    Created the files for the project 2 simulations.

---
+++ trunk/src/project2/CheckoutCounter.scala
@@ -0,0 +1,10 @@
+package project2
+
+import simulation._
+
+/** @see Grocery Store Checkout Counter: Example 3.3: Page 77
+ *  @author mepcotterell@gmail.com
+ */
+object CheckoutCounter extends App with EventSchedulingSimulation {
+  
+}
---
+++ trunk/src/project2/DumpTrucks.scala
@@ -0,0 +1,10 @@
+package project2
+
+import simulation._
+
+/** @see Dump Trucks for Hauling Coal: Example 3.5: Page 81
+ *  @author mepcotterell@gmail.com
+ */
+object DumpTrucks extends App with EventSchedulingSimulation {
+
+}
