a49a2a597570228d93d6250a6911538332170ec1
mepcotterell <mepcotterell@gmail.com>
2011-08-31 20:59:46 +0000

    Added the CallCenter example

--- src/simulation/Event.scala
+++ src/simulation/Event.scala
@@ -10,19 +10,24 @@ abstract class Event (entity: Entity) (implicit scheduler: Scheduler) extends Or
   // the event time
   var time: Int = 0
   
+  /** The current time in the simulation
+   *  @author mepcotterell@gmail.com
+   */
+  def clock = scheduler.clock
+  
   // mandatory implementation of Ordered.compare
   def compare (that: Event) = (that.time - this.time)
+  
+  /** The event logic
+   *  @author mepcotterell@gmail.com
+   */
+  def occur: Unit
   
   /** Schedules a future event using the implicitly associated Scheduler
    *  @author mepcotterell@gmail.com
    *  @param event the future event
    *  @param delay the delay between now and when the event should occur
    */
-  def schedule (event: Event, delay: Int) = scheduler.schedule(event, delay)
-  
-  /** The event logic
-   *  @author mepcotterell@gmail.com
-   */
-  def occur: Unit
+  def schedule (event: Event, delay: Double) = scheduler.schedule(event, delay.toInt)
   
 } // abstract class Event
