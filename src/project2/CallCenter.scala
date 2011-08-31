e948ea5c7c9d5879a4630397497df734abdcc690
mepcotterell <mepcotterell@gmail.com>
2011-08-31 22:53:38 +0000

    Created the files for the project 2 simulations.

---
+++ src/project2/CallCenter.scala
@@ -0,0 +1,10 @@
+package project2
+
+import simulation._
+
+/** @see Able-Baker Call Center: Example 3.1: Page 69
+ *  @author mepcotterell@gmail.com
+ */
+object CallCenter extends App with EventSchedulingSimulation {
+  
+}
