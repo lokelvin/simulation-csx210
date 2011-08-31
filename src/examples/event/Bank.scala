f70eb7a92711a9cfe723cc5512c83342024234da
mepcotterell <mepcotterell@gmail.com>
2011-08-31 22:42:37 +0000

    Added the Bank simulation example from class.

--- src/examples/event/Bank.scala
+++ src/examples/event/Bank.scala
@@ -10,9 +10,94 @@ object Bank extends App with EventSchedulingSimulation {
   // Assign values to the Simulation variables
   tStart = 0
   tStop  = 100
-  λ      = 9.0
-  μ      = 2.0
+  λ      = 10.0
+  μ      = 4.0
   
-  // @TODO finish this
+  // total number of tellers in the bank
+  val N = 1
   
+  // the number of customers in the bank
+  var L = 0
+  
+  // the total number of customers serviced
+  var nServiced = 0
+  
+  // the service times
+  var serviceTimes = scala.collection.mutable.ListBuffer.empty[Double]
+  
+  /** Represents a customer in the bank
+   *  @author mepcotterell@gmail.com
+   */
+  case class Customer extends Entity
+
+  /** The Arrival Event
+   *  @author mepcotterell@gmail.com
+   *  @param person the Customer associated with the event
+   */
+  case class Arrival (customer: Customer) extends Event (customer) {
+    def occur {
+           
+      if (clock <= tStop) {
+        
+        // schedule a new arrival
+        schedule(Arrival(Customer()), Rand(1.0 / λ))
+        
+      } // if
+      
+      if (L < N) {
+        
+        // the service time
+        val delay = Rand(1.0 / μ)
+        
+        // add to the set of service times
+        serviceTimes += delay
+        
+        // schedule a departure
+        schedule(Departure(customer), delay)
+        
+      } // if
+      
+      // increase the number of customers in the bank
+      L += 1
+      
+    } // def occur
+  } // case class MakeCall
+  
+  /** The Departure Event
+   *  @author mepcotterell@gmail.com
+   *  @param person the Customer associated with the event
+   */
+  case class Departure (customer: Customer) extends Event (customer) {
+    def occur {
+      
+      // increase the number of customers serviced
+      nServiced += 1
+      
+      if (L > N) {
+      
+        // schedule a departure
+        schedule(Departure(customer), Rand(1.0 / μ))
+      
+      } // if
+      
+      // decrease the number of customers in the bank
+      L -= 1
+      
+    } // def occur
+  } // case class HangUp
+  
+  // schedule the first event
+  schedule(Arrival(Customer()), tStart + Rand(1.0 / λ))
+  
+  // run the simulation
+  simulate
+  
+  // compute the average service time
+  val avgServiceTime = serviceTimes.reduceLeft(_+_) / serviceTimes.size
+  
+  // print out some information
+  println
+  println("The total number of customers serviced was %s".format(nServiced))
+  println("The total average service time was %s".format(avgServiceTime))
+
 } // object Bank
