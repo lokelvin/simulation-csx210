7f9fe676125935f2cee1f7d46082a02ffd924dea
mepcotterell@gmail.com <mepcotterell@gmail.com@bfff1a8a-6af3-b283-1508-272d5eb36651>
2011-09-07 22:06:57 +0000

    Started some preliminary work on process interaction simulation.

--- trunk/src/simulation/process/Model.scala
+++ trunk/src/simulation/process/Model.scala
@@ -3,7 +3,12 @@ package process
 
 import scala.actors._
 import scala.actors.Actor._
+import scala.collection.mutable.PriorityQueue
 
 abstract class Model extends Actor {
 
-}
+  // The future events list
+  val futureEvents = PriorityQueue.empty[SimActor]
+  
+  
+} // class Model
