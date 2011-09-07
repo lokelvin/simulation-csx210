0d6f82bcd3877d4a445956de78a3039dee96a7de
mepcotterell@gmail.com <mepcotterell@gmail.com@bfff1a8a-6af3-b283-1508-272d5eb36651>
2011-09-07 22:00:34 +0000

    Set up some stuff to prepare for process interaction simulation.

---
+++ trunk/src/simulation/process/Model.scala
@@ -0,0 +1,9 @@
+package simulation
+package process
+
+import scala.actors._
+import scala.actors.Actor._
+
+abstract class Model extends Actor {
+
+}
---
+++ trunk/src/simulation/process/ProcessInteractionSimulation.scala
@@ -0,0 +1,11 @@
+package simulation
+package process
+
+/** Provides useful variables and functions for process interaction simulation
+ *  @author mepcotterell@gmail.com
+ */
+trait ProcessInteractionSimulation extends Simulation {
+
+  println("Setting up the environment for a process interaction simulation [trait ProcessInteractionSimulation]")
+  
+}
