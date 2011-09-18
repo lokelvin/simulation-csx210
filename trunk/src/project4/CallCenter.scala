731e0eefe8b3bd3b6e5f5902066f6aa00d0163dc
rbedger <rbedger@bfff1a8a-6af3-b283-1508-272d5eb36651>
2011-09-18 00:17:59 +0000

    process interaction

--- trunk/src/project4/CallCenter.scala
+++ trunk/src/project4/CallCenter.scala
@@ -12,35 +12,103 @@ import project4.CallCenter.Director
 object CallCenter extends App with ProcessInteractionSimulation {
 
   class Director() extends Model
+
   implicit val director = new Director
 
-  case class caller() extends SimActor() {
+  val N_SERVERS = 2
+
+  var N_CALLS = 0
+
+  var waitQ : Queue[caller] =  new Queue[caller]
+
+  // Assign values to the Simulation variables
+  tStart = 0
+  tStop  = 100
+  λDist = Map[Int,Double](1->0.25,2->0.65,3->0.85,4->1.0)
+  μDist = Map[Int,Double](2->0.30,3->0.58,4->0.83,5->1.0)
+  μ2Dist = Map[Int,Double](3->0.35,4->0.60,5->0.80,6->1.0)
+
+  var L = 0
+
+  case class Operator() extends Entity {
+    var idle = true
+  }
+
+  var able = Operator()
+  var baker = Operator()
+
+  case class caller(callerNumber : Int) extends SimActor() {
+    var arrivalTime : Int = 0
+
     actions.push("leave","usePhone","arrive")
+
+    var myOperator : Operator = null
+
+    def useOperator(operator : Operator) {
+      operator.idle = false
+      this.myOperator = operator
+    }
+
+    def releaseOperator() {
+      myOperator.idle = true
+      println("L value = %d".format(L))
+      if (L > N_SERVERS) {
+        val actor = waitQ.dequeue
+        director.schedule(actor,0,actor.actions.top)
+        println("Caller %d dequeued for %s, waited %d".format(actor.callerNumber,actor.actions.top,director.clock-actor.arrivalTime))
+      }
+      L = L - 1
+    }
+
     def act() {
          while (true) {
            actions.pop match {
              case "arrive" => {
-               director.schedule(caller(),10)
-               println("Im here!")
-               director.schedule(this,5)
+               arrivalTime = director.clock
+               N_CALLS = N_CALLS + 1
+               director.schedule(caller(N_CALLS),DiscreteRand(λDist).toInt,"arrive")
+               L = L + 1
+               if (L > N_SERVERS) {
+                 waitQ.enqueue(this)
+               }
+               else {
+                 actions.pop
+                 if (able.idle){
+                   useOperator(able)
+                   director.schedule(this,DiscreteRand(μDist).toInt,actions.top)
+                 }
+                 else if (baker.idle){
+                   useOperator(baker)
+                   director.schedule(this,DiscreteRand(μ2Dist).toInt,actions.top)
+                 }
+
+               }
              }
              case "usePhone" => {
-               println("Im using the phone!")
-               director.schedule(this,5)
+                if (able.idle){
+                   useOperator(able)
+                   director.schedule(this,DiscreteRand(μDist).toInt,actions.top)
+                 }
+                 else if (baker.idle){
+                   useOperator(baker)
+                   director.schedule(this,DiscreteRand(μ2Dist).toInt,actions.top)
+                 }
              }
              case "leave" => {
-               println("Bye!")
+               releaseOperator()
+               director !"resume directing"
+               exit()
              }
            }
           director ! "resume directing"
-          receive {case "resume acting" => {println("I'll resume")}}
+          receive {case "resume acting" => {println("\t\t%d [action] Person %d is %s".format(director.clock,this.callerNumber,this.actions.top))}}
           }
          }
     }
 
 
-
-    director.schedule(caller(),0)
+    val actor = caller(N_CALLS)
+    director.schedule(actor,0,actor.actions.top)
     director.start
 
 }
