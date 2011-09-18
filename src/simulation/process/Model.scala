37e01f0bed398ef5732f2192e22519799b640fb8
rbedger <rbedger@bfff1a8a-6af3-b283-1508-272d5eb36651>
2011-09-18 00:17:59 +0000

    process interaction

--- src/simulation/process/Model.scala
+++ src/simulation/process/Model.scala
@@ -13,7 +13,7 @@ abstract class Model extends Actor {
   val futureEvents = PriorityQueue.empty[SimActor]
   var simulating = true
 
-  def schedule (actor : SimActor, delay: Int) {
+  def schedule (actor : SimActor, delay: Int, nextState : String) {
 
     actor.actTime = clock + delay
     // add the event to the list
@@ -21,13 +21,15 @@ abstract class Model extends Actor {
 
     // print a nice message
     Console.BLUE
-    println("%10s %10s Scheduled %s for t = %s".format(clock, "[action]", actor, actor.actTime))
+    println("%10s %10s Scheduled %s for t = %s for %s".format(clock, "[action]", actor, actor.actTime,nextState))
     Console.RESET
 
   } // def schedule
 
   def act() {
     while (simulating && !futureEvents.isEmpty) {
+      if (clock > 100)
+        simulating = false
       val actor = futureEvents.dequeue()
       clock = actor.actTime
       if (actor.yetToAct) {
@@ -37,7 +39,7 @@ abstract class Model extends Actor {
       else {
         actor ! "resume acting"
       }
-      receive {case "resume directing" => {println("Director received message!")}}
+      receive {case "resume directing" => {}}
     }
   }
   
